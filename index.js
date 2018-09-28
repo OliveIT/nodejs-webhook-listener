15db836324133a928dccfe6e50aa009d0b41a954
captainheart <captainheart@outlook.com>
2018-09-28 20:05:06 +1000

    write to file

--- index.js
+++ index.js
@@ -1,18 +1,27 @@
 const express = require('express')
 const bodyParser = require('body-parser')
+const fs = require('fs');
+const moment = require('moment');
 
 // Create a new instance of express
 const app = express()
 
+app.use(express.static(__dirname + '/public'));
+
 // Tell express to use the body-parser middleware and to not parse extended bodies
 app.use(bodyParser.urlencoded({ extended: false }))
-
 // Route that receives a POST request to /sms
 app.post('/webhook', function (req, res) {
-  const body = req.body.Body
+  const body = req.body
+  var data = "=====  " + moment().format('MMMM Do YYYY, h:mm:ss a') + "  =====\n" + JSON.stringify(body) + "\n\n"
   res.set('Content-Type', 'text/plain');
-  res.send(`${body}`);
-  console.log(req.body);
+  res.send(data);
+  
+  console.log(data);
+  
+  fs.appendFile('public/message.txt', data, function (err) {
+    if (err) throw err;
+  });
 })
 
 // Tell our app to listen on port 3000
@@ -23,14 +32,3 @@ app.listen(37037, function (err) {
 
   console.log('Server started on port 37037')
 });
-
-/*
-POST https://localhost:37037/webhook HTTP/1.1
-content-type: application/json
-
-{
-    "name": "sample",
-    "time": "Wed, 21 Oct 2015 18:27:50 GMT"
-}
-
-*/
